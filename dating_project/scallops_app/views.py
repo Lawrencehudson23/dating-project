a7b728183f3d5e495a8cc622fee0581e3eb14782
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-01-29 15:09:17 -0800

    new html

--- dating_project/scallops_app/views.py
+++ dating_project/scallops_app/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def about_us(request):
+    return render(request,'about_us.html')
